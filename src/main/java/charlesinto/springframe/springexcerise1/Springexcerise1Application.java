8ee97251aa2d84be4baed47cd4864c817f329325
charles <charles.onuorah@yahoo.com>
2022-11-10 18:25:41 +0100

    Initial commit

---
+++ src/main/java/charlesinto/springframe/springexcerise1/Springexcerise1Application.java
@@ -0,0 +1,13 @@
+package charlesinto.springframe.springexcerise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springexcerise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springexcerise1Application.class, args);
+	}
+
+}
---
+++ src/test/java/charlesinto/springframe/springexcerise1/Springexcerise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package charlesinto.springframe.springexcerise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springexcerise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
